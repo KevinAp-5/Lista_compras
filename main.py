4563a1bff384b6c8b7bea468f1b6648057ec49d5
KevinAp-5 <keven.clash3@gmail.com>
2022-01-10 21:30:16 -0300

    Main classes
    Carrinho vai receber a classe produto, que vai ser a responsavel pela criacao de cada produto. Carrinho tem funcoes de adicionar e mostrar

---
+++ main.py
@@ -0,0 +1,30 @@
+class Carrinho:
+    def __init__(self):
+        self.produtos = list()
+
+    def adicionar(self, produto):
+        if type(produto.produto) == dict:
+            self.produtos.append(produto.produto)
+
+    def mostrar(self):
+        if len(self.produtos) == 0:
+            print('Adicione um item no carrinho primeiro.')
+            return
+
+        for produto in self.produtos:
+            for nome, valor in produto.items():
+                print(f'Produto: {nome}\nValor: {valor:.2f}')
+            print('-'*50)
+
+
+class Produto:
+    def __init__(self, nome, preco=0.0):
+        self.produto = {str(nome).capitalize(): float(preco)}
+
+
+if __name__ == "__main__":
+    carrinho = Carrinho()
+    carrinho.mostrar()
+    carrinho.adicionar(Produto('celular xiaomi redmi note8 64gb', 1299.99))
+    carrinho.adicionar(Produto('teste', 420.69))
+    carrinho.mostrar()
